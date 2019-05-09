4a6230716304d76cc86bf2351246a054629a6978
Thomas Dau <ThomasQDau@gmail.com>
2019-05-08 19:49:39 -0700

    xfering to desktop

---
+++ public/assets/js/js.js
@@ -0,0 +1,16 @@
+$( document ).ready(function () {
+
+
+    $(document).on('click', '.noteBtn', function () {
+        
+    })
+})  
+
+
+// $.ajax({
+//     type: "POST",
+//     url: "/update",
+//     data: data,
+//     success: success,
+//     dataType: dataType
+//   });
---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+// var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+var app = express();
+// app.use(logger("dev"));
+var exphbs = require("express-handlebars");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/scrape", function (req, res) {
+  axios.get("https://boards.na.leagueoflegends.com/en/").then(function (response) {
+    var $ = cheerio.load(response.data);
+    $(".discussion-list-item").each(function (i, element) {
+      var result = {
+          title: $(this).children('.title').children('.discussion-title').children().children().text(),
+          body: $(this).children('.title').children('.discussion-title').children('.title-link').children('.title-span').attr('title'),
+          link: 'https://boards.na.leagueoflegends.com' + $(this).attr('data-href')
+      };
+      db.Articles.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/", function (req, res) {
+  db.Articles.find({}).populate('notes').then(function (data) {
+    console.log(data[0]);
+    res.render("index", {
+        Articles: data
+    });
+  })
+});
+
+// app.get("/articles/:id", function (req, res) {
+//   db.Article.findOne({ _id: req.params.id}).populate('note').then(function (data) {
+//     res.json(data);
+//   })
+// });
+
+// app.post("/articles/:id", function (req, res) {
+//   db.Note.create(req.body).then(function (data) {
+//     console.log('Data 1: ', data);
+//     db.Article.updateOne({_id: req.params.id}, {$set: {note: data._id}}).then(function(data2) {
+//       console.log('Data 2: ', data2);
+//       res.end();  
+//     })
+//   })
+// });
+
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
